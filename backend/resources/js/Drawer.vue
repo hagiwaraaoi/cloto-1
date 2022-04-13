6267df5817848826b70790b89077fe73efa19e25
miyata <itsuki.110725@gmail.com>
2022-04-13 14:26:10 +0900

    メニューバー調整

--- backend/resources/js/Drawer.vue
+++ backend/resources/js/Drawer.vue
@@ -23,49 +23,7 @@
         </v-list-item-content>
       </v-list-item>
 
-      <v-list-item exact :to="{ name: 'floor', params: { roomId: 1 } }">
-        <v-list-item-icon>
-          <v-icon>mdi-AccountSchool</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>GROW×VERSE 1</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <div class="pa-2" v-if="$route.name === 'floor'">
-        <v-btn
-          depressed
-          block
-          color="#f6bf00"
-          dark
-          :to="{ name: 'room', params: { roomId: $route.params.roomId } }"
-          :disabled="authUser.seat !== null"
-        >
-          入室
-        </v-btn>
-      </div>
-
-      <v-list-item exact :to="{ name: 'floor', params: { roomId: 2 } }">
-        <v-list-item-icon>
-          <v-icon>mdi-AccountSchoolOutline</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>GROW×VERSE 2</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <div class="pa-2" v-if="$route.name === 'floor'">
-        <v-btn
-          depressed
-          block
-          color="#f6bf00"
-          dark
-          :to="{ name: 'room', params: { roomId: $route.params.roomId } }"
-          :disabled="authUser.seat !== null"
-        >
-          入室
-        </v-btn>
-      </div>
-
-      <!-- <v-list-item exact :to="{ name: 'mystudy' }">
+      <v-list-item exact :to="{ name: 'mystudy' }">
         <v-list-item-icon>
           <v-icon>mdi-note-text</v-icon>
         </v-list-item-icon>
@@ -82,6 +40,41 @@
           <v-list-item-title>マイページ</v-list-item-title>
         </v-list-item-content>
       </v-list-item> -->
+      <v-list-item-group color="success">
+        <v-list-item-icon>
+          <v-icon>mdi-AccountSchoolOutline</v-icon>
+        </v-list-item-icon>
+        <v-tooltip bottom v-for="floor in floors.slice().reverse()" :key="floor.id">
+          <template v-slot:activator="{ on, attrs }">
+            <v-list-item
+              class="mb-4"
+              v-bind="attrs"
+              v-on="on"
+              :to="{ name: 'floor', params: { roomId: floor.id } }"
+            >
+              <v-list-item-content>
+                <v-list-item-title class="text-body-1 font-weight-bold">
+                  GROW×VERSE {{ floor.id }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+
+          <span>{{ floor.userNum }}人在室中</span>
+        </v-tooltip>
+      </v-list-item-group>
+      <div class="pa-2" v-if="$route.name === 'floor'">
+      <v-btn
+        depressed
+        block
+        color="#f6bf00"
+        dark
+        :to="{ name: 'room', params: { roomId: $route.params.roomId } }"
+        :disabled="authUser.seat !== null"
+      >
+        入室
+      </v-btn>
+    </div>
     </v-list>
 
     <template v-slot:append>
@@ -95,6 +88,7 @@
 <script>
 export default {
   props: {
+    // floors: Array,
     value: {
       type: Boolean,
       default: false,
@@ -107,6 +101,31 @@ export default {
     authUser() {
       return this.$store.getters['auth/user'];
     },
+  },
+  data() {
+    return {
+      floors: [], // ドロワーメニュー用階層一覧
+    };
+  },
+  async mounted() {
+    let response = await axios.get('/api/rooms');
+    response.data.forEach((room) => {
+      // 着席者数のカウント
+      let userNum = 0;
+      room.sections.forEach((section) => {
+        section.seats.forEach((seat) => {
+          if (seat.user) {
+            userNum += 1;
+          }
+        });
+      });
+      this.floors.push({
+        id: room.id,
+        name: room.name,
+        userNum: userNum
+      });
+    });
+    return;
   },
 };
 </script>
